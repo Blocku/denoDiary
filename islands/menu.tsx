7604bfe44e612f1cf0c41c81b20f6e42ad73c6db
Blocku <126176292+Blocku@users.noreply.github.com>
2024-09-20 19:25:46 +0500

    Add time create todo and add more subjects

--- islands/menu.tsx
+++ islands/menu.tsx
@@ -7,7 +7,7 @@ export default function Menu() {
     const [newTodo, setNewTodo] = useState<Object>({subject: '', paragraph: '', page: '', number: ''})
     const [todos, setTodos] = useState<Array>([])
 
-    const subjects: Array<string> = ['Русский язык', 'Физика', 'Английский язык', 'История', 'Химия', 'Биология', 'Математика', 'География']
+    const subjects: Array<string> = ['Русский язык', 'Физика', 'Английский язык', 'История', 'Химия', 'Биология', 'Алгебра', 'География', 'Информатика', 'Геометрия', 'Обществознание']
 
     useEffect(() => {
         const storageTodos = localStorage.getItem('todos')
@@ -16,7 +16,7 @@ export default function Menu() {
         }
       }, [])
  
-    function createTask(e) {
+    function createTodo(e) {
         e.preventDefault()
         if(newTodo.subject && newTodo.paragraph !== ''){
             const todo = {
@@ -24,7 +24,8 @@ export default function Menu() {
                 paragraph: newTodo.paragraph,
                 page: newTodo.page ? ( ' / ' + newTodo.page) : null,
                 number: newTodo.number ? (' / ' + newTodo.number) : null,
-                id: Date.now()
+                id: Date.now(),
+                date: (new Date().toDateString())
             }
             setTodos([...todos, todo])
             setOpenMenu(false)
@@ -34,7 +35,7 @@ export default function Menu() {
         }
     }
 
-    function deleteTask(id: Date){
+    function deleteTodo(id: Date){
         const newTodos = todos.filter(todo => {
           if(todo.id !== id){
             return todo
@@ -43,6 +44,11 @@ export default function Menu() {
         setTodos(newTodos)
         localStorage.setItem('todos', JSON.stringify(newTodos))
       }
+
+    // function changeTodo(todo: Object) {
+    //     setNewTodo(todo)
+    //     setOpenMenu(true)
+    // }
 
 
     return(
@@ -57,13 +63,21 @@ export default function Menu() {
 
                 <ul className='space-y-3' >
                     {todos && todos.map(todo => 
-                    <li key={todo.id} className='w-full ring-2 ring-zinc-300 rounded-md p-1' >
+                    <li key={todo.id} className='w-full ring-2 ring-zinc-300 rounded-md p-1 select-none' >
                         <span className='text-lg font-medium' >{todo.subject}</span>
                         <p className='text-sm text-zinc-500' >{todo.paragraph}{todo.page}{todo.number}</p>
-                        <div className='flex justify-end items-center' >
-                            <svg onClick={() => deleteTask(todo.id)} className="h-6 w-6 fill-none stroke-2 stroke-red-500 cursor-pointer">
-                            <path d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                            </svg>
+                        <div className='flex justify-between items-center mt-1' >
+                            <span className='text-sm text-zinc-500' >{todo.date}</span>
+
+                            <span className='flex' >
+                                <svg className="h-6 w-6 stroke-2 stroke-zinc-400 fill-none cursor-pointer">
+                                    <path d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                </svg>
+                                <svg onClick={() => deleteTodo(todo.id)} className="h-6 w-6 fill-none stroke-2 stroke-red-500 cursor-pointer">
+                                    <path d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                </svg>
+                            </span>
+                            
                         </div>
                     </li>
                     )}
@@ -74,7 +88,7 @@ export default function Menu() {
             {openMenu && 
                 <section className='absolute top-0 w-full' >
                     <div onClick={() => setOpenMenu(false)} className='absolute w-dvw h-dvh bg-black/30 z-10' ></div>
-                    <form onSubmit={createTask} className='absolute rounded-md z-20 w-[94dvw] md:max-w-screen-md mx-auto right-0 left-0 top-[35dvh] bg-white shadow-sm p-3 space-y-5' >
+                    <form onSubmit={createTodo} className='absolute rounded-md z-20 w-[94dvw] md:max-w-screen-md mx-auto right-0 left-0 top-[35dvh] bg-white shadow-sm p-3 space-y-5' >
                         <label>
                             <input value={ newTodo.subject ? newTodo.subject : 'Выбери предмет'} onClick={() => setOpenMenuSubject(!openMenuSubject)} type='button' className='w-full text-left font-medium ring-zinc-200 ring-2 rounded p-1' />
                             <svg className="absolute top-4 right-4 h-6 w-6 stroke-2 stroke-zinc-700 fill-none">
@@ -84,9 +98,9 @@ export default function Menu() {
                             {openMenuSubject && 
                                 <ul className='absolute bg-white ring-2 ring-zinc-200 rounded mt-1.5 p-1 left-3 right-3 mx-auto' >
                                     {subjects.map((subject : string) => 
-                                    <button key={subject} onClick={() => (setOpenMenuSubject(false), setNewTodo({...newTodo, subject: subject}))} className='font-medium w-full text-left hover:bg-zinc-100' >
+                                    <li key={subject} onClick={() => (setNewTodo({...newTodo, subject: subject}))} className='font-medium w-full text-left hover:bg-zinc-100' >
                                         {subject}
-                                    </button>
+                                    </li>
                                     )}
                                 </ul>
                             }
